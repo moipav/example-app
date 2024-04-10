4783f94e2409be7c7c9bd5d6efee6e6601b18a90
moipav <moipav@ya.ru>
2024-04-10 12:28:03 +0800

    add image and layout for about and main page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title')
+About page 2
+@endsection
+
+@section('content')
+<h1>This is about page</h1>
+@endsection
---
+++ resources/views/main.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center fw-bolder">Главная страница галереи</h1>
+        <div class="row">
+            <div class="col-md-3">
+                <img src="/rand.jpg" width="256" alt="pencil">
+            </div>
+            <div class="col-md-3">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi commodi, cumque et fugiat, laborum neque nostrum perferendis perspiciatis placeat quisquam quos repudiandae rerum similique velit vitae voluptates voluptatum. Perferendis, quidem?
+            </div>
+            <div class="col-md-3">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi commodi, cumque et fugiat, laborum neque nostrum perferendis perspiciatis placeat quisquam quos repudiandae rerum similique velit vitae voluptates voluptatum. Perferendis, quidem?
+            </div>
+            <div class="col-md-3">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi commodi, cumque et fugiat, laborum neque nostrum perferendis perspiciatis placeat quisquam quos repudiandae rerum similique velit vitae voluptates voluptatum. Perferendis, quidem?
+            </div>
+
+        </div>
+    </div>
+@endsection
